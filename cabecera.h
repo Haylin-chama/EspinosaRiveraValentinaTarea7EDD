8bdc9b4be826abfa06fbe0df4bcdfbe1357daeb9
Haylin Ellenes <haylin-chama@hotmail.es>
2014-10-23 17:53:29 -0300

    Update cabecera.h
    Modificado y listo.

--- cabecera.h
+++ cabecera.h
@@ -1,20 +1,23 @@
-#ifndef CABECERA_H_INCLUDED
-#define CABECERA_H_INCLUDED
+#ifndef CAB7_H_INCLUDED
+#define CAB7_H_INCLUDED
 #include <iostream>
 
 using namespace std;
 
-typedef struct Nodo{
+typedef struct Mesa{
     string nombre;
     string titulo;
-    struct Nodo *ant;
-    struct Nodo *sig;
-};
+    int exp;
+    struct Mesa *ant;
+    struct Mesa *sig;
+}Mesa_s;
 
-typedef Nodo *Lista;
+typedef Mesa *Lista;
 
-void agregar(Lista &, int );
+void Agregar(Lista &);
 void Listar(Lista );
+void Eliminar(Lista &, int , Lista &, Lista &, Lista &);
+void Welta(Lista &,int ,int , Lista &, Lista &, Lista &);
+void Elegido(Lista &, int );
 
-
-#endif // CABECERA_H_INCLUDED
+#endif // CAB7_H_INCLUDED
