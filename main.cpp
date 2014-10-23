4e4fa50e09fc6ed6c9f78946ab3327f351676c9d
Haylin Ellenes <haylin-chama@hotmail.es>
2014-10-23 17:59:20 -0300

    Update main.cpp
    Main a medias

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main():
 {
-  
-  
+   cout << "BIENVENIDO REY ARTURO" << endl;
+    cout<<"     M   E   N   U:  "<<endl;
+    Mesa_s M;
+    Agregar(M);
+    Lista p,q,r;
 }
