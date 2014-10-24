116cae22d119eed4c8633228c90d0a600c6a0262
Haylin Ellenes <haylin-chama@hotmail.es>
2014-10-24 14:13:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
-#include <iostream>
-
 using namespace std;
 
-int main():
+int main()
 {
-   cout << "BIENVENIDO REY ARTURO" << endl;
-    cout<<"     M   E   N   U:  "<<endl;
-    Mesa_s M;
-    Agregar(M);
-    Lista p,q,r;
+
+   system("color f0");//Se agrega color al sistema
+    char opcion;
+    int n,k;
+    Lista M,p,q,r;
+    do
+    {
+        system("cls");
+        cout<<"	BIENVENIDO REY ARTURO\n\n   M   E   N   U\n";
+        cout<<"1) Generar los N caballeros de la mesa redonda"<<endl;
+        cout<<"2) Mostrar toda la información contenida en la mesa redonda"<<endl;
+        cout<<"3) Solicitar el valor de K, y elegir el nombre del caballero elegido"<<endl;
+        cout<<"0) Salir"<<endl;
+        cout<<endl<<"Ingrese una opcion: ";
+        cin>>opcion;
+        switch(opcion){
+            case '1':{
+                cout <<"Numero de caballeros en la mesa: "<<endl;
+                n=Validar();
+                Agregar(M,n);
+                break;
+            }
+             case '2':{
+                cout<<"CABALLEROS PRESENTES\n";
+                p=M;
+                Listar2(M,p);
+                break;
+            }
+             case '3':{
+                cout<<"Valor de k: ";
+                k=Validar();
+                Elegido(M,k);
+                break;
+            }
+        }
+
+    }while(opcion!='0');
+
 }
